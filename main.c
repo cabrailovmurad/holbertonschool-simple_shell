df1bdd3a66e587542cb8b25476fb6dbee5ed26f8
cabrailovmurad <dmurad908@gmail.com>
2025-08-21 19:03:16 +0000

    Add task 2

--- main.c
+++ main.c
@@ -4,37 +4,14 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
-#include <ctype.h>
 
 extern char **environ;
-
-/* Функция для удаления ведущих и конечных пробелов */
-char *trim_spaces(char *str)
-{
-    char *end;
-
-    while (isspace((unsigned char)*str))
-        str++;
-
-    if (*str == 0)
-        return str;
-
-    end = str + strlen(str) - 1;
-    while (end > str && isspace((unsigned char)*end))
-        end--;
-
-    *(end + 1) = '\0';
-
-    return str;
-}
 
 int main(void)
 {
     char *line = NULL;
     size_t len = 0;
     ssize_t nread;
-    pid_t pid;
-    int status;
 
     while (1)
     {
@@ -52,28 +29,33 @@ int main(void)
         if (line[nread - 1] == '\n')
             line[nread - 1] = '\0';
 
-        char *cmd = trim_spaces(line);
-        if (cmd[0] == '\0')
-            continue;
-
-        pid = fork();
-        if (pid == 0)
+        char *cmd = strtok(line, " ");
+        while (cmd != NULL)
         {
-            char *argv[] = {cmd, NULL};
-            if (execve(argv[0], argv, environ) == -1)
+            pid_t pid = fork();
+            int status;
+
+            if (pid == -1)
             {
-                perror("./hsh");
+                perror("fork");
+                free(line);
                 exit(EXIT_FAILURE);
             }
-        }
-        else if (pid > 0)
-        {
-            wait(&status);
-        }
-        else
-        {
-            perror("fork");
-            exit(EXIT_FAILURE);
+            if (pid == 0)
+            {
+                char *argv[] = {cmd, NULL};
+                if (execve(argv[0], argv, environ) == -1)
+                {
+                    perror("./hsh");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else
+            {
+                wait(&status);
+            }
+
+            cmd = strtok(NULL, " ");
         }
     }
 
