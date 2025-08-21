1c594253c810acaf663f32c6e1bc5270f35a2e0b
cabrailovmurad <dmurad908@gmail.com>
2025-08-21 19:23:28 +0000

    task3

--- main.c
+++ main.c
@@ -4,95 +4,76 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
-#include <ctype.h>
 
 extern char **environ;
-
 #define MAX_ARGS 100
+#define MAX_PATH 1024
 
-/* Function to remove leading and trailing spaces */
 char *trim_spaces(char *str)
 {
     char *end;
 
-    while (*str && isspace(*str))  /* skip leading spaces */
+    while (*str == ' ' || *str == '\t' || *str == '\n')
         str++;
 
     if (*str == 0)
         return str;
 
     end = str + strlen(str) - 1;
-    while (end > str && isspace(*end))  /* remove trailing spaces */
+    while (end > str && (*end == ' ' || *end == '\t' || *end == '\n'))
         end--;
 
     *(end + 1) = '\0';
     return str;
 }
 
+char *find_command(char *cmd)
+{
+    char *path_env;
+    char *path_copy;
+    char *dir;
+    static char full_path[MAX_PATH];
+
+    if (!cmd)
+        return NULL;
+
+    if (strchr(cmd, '/'))
+        return cmd;
+
+    path_env = getenv("PATH");
+    if (!path_env)
+        return NULL;
+
+    path_copy = strdup(path_env);
+    if (!path_copy)
+        return NULL;
+
+    dir = strtok(path_copy, ":");
+    while (dir != NULL)
+    {
+        snprintf(full_path, MAX_PATH, "%s/%s", dir, cmd);
+        if (access(full_path, X_OK) == 0)
+        {
+            free(path_copy);
+            return full_path;
+        }
+        dir = strtok(NULL, ":");
+    }
+
+    free(path_copy);
+    return NULL;
+}
+
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
+    char *line;
+    size_t len;
     ssize_t nread;
     pid_t pid;
     int status;
     char *args[MAX_ARGS];
     int i;
+    char *cmd_path;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            write(1, "$ ", 2);
-
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            if (isatty(STDIN_FILENO))
-                write(1, "\n", 1);
-            break;
-        }
-
-        if (line[nread - 1] == '\n')
-            line[nread - 1] = '\0';
-
-        line = trim_spaces(line);
-
-        if (*line == '\0')  /* empty line */
-            continue;
-
-        /* split the line into arguments */
-        args[0] = strtok(line, " ");
-        if (args[0] == NULL)
-            continue;
-
-        for (i = 1; i < MAX_ARGS; i++)
-        {
-            args[i] = strtok(NULL, " ");
-            if (args[i] == NULL)
-                break;
-        }
-
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("fork");
-            free(line);
-            exit(EXIT_FAILURE);
-        }
-        if (pid == 0)
-        {
-            if (execve(args[0], args, environ) == -1)
-            {
-                perror("./hsh");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            wait(&status);
-        }
-    }
-
-    free(line);
-    return 0;
-}
+    line = NULL;
+    l
